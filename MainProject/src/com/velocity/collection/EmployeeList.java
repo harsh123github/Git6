04e1939b2129d51cb9779eebe31c8aedb4f1cd53
Harshal nalawade <harshalnalawade2330@gmail.com>
2024-06-27 16:57:16 +0530

    Formatted and removed unwanted import in EmployeeList.java

--- MainProject/src/com/velocity/collection/EmployeeList.java
+++ MainProject/src/com/velocity/collection/EmployeeList.java
@@ -6,15 +6,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class EmployeeList {
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		List<Employee> emplist = new ArrayList<>();// loosely coupled
-		emplist.add(new Employee(101, "Nayan",20000,"pune"));
-		emplist.add(new Employee(102, "pratik",60000,"yavatmal"));
-		emplist.add(new Employee(103, "Aayan",50000,"mumbai"));
-		emplist.add(new Employee(104, "Rahul",40000,"delhi"));
-		emplist.add(new Employee(105, "Akky",30000,"pune"));
-		System.out.println("before sorting" +emplist);
-		Collections.sort(emplist, new SalaryComparator());//new salary is instance of comparator
+		emplist.add(new Employee(101, "Nayan", 20000, "pune"));
+		emplist.add(new Employee(102, "pratik", 60000, "yavatmal"));
+		emplist.add(new Employee(103, "Aayan", 50000, "mumbai"));
+		emplist.add(new Employee(104, "Rahul", 40000, "delhi"));
+		emplist.add(new Employee(105, "Akky", 30000, "pune"));
+		System.out.println("before sorting" + emplist);
+		Collections.sort(emplist, new SalaryComparator());// new salary is instance of comparator
 		System.out.println("After sorting based on salary" + emplist);
 	}
 
